619955e269f45fe276b2b4ee1c48e3e9d79701a9
root <fukumori@partner-prop.com>
2022-05-15 17:33:07 +0900

    create to get Books api

--- app/Http/Controllers/BookApiController.php
+++ app/Http/Controllers/BookApiController.php
@@ -8,7 +8,8 @@ use App\Models\Book;
 class BookApiController extends Controller
 {
     public function getBooks() {
-
+        $books = Book::get()->toJson(JSON_PRETTY_PRINT);
+        return response($books, 200);
     }
 
     public function createBook(Request $request) {
